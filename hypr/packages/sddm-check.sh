accabc40b16d1649155f51956bb9f99fa633b893
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-04-22 15:20:13 +0100

    update

---
+++ hypr/packages/sddm-check.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+figlet -f 3d "sddm check"
+sh ~/endeavour-dots/scripts/rm-dm-managers.sh
+echo ""
+if [ ! -d /etc/sddm.conf.d/ ]; then
+    sudo mkdir /etc/sddm.conf.d
+    echo "Folder /etc/sddm.conf.d created."
+fi
+sudo cp ~/endeavour-dots/sddm/sddm.conf /etc/sddm.conf.d/
+echo "File /etc/sddm.conf.d/sddm.conf updated."
+echo ""
---
+++ scripts/wal.sh
@@ -0,0 +1,11 @@
+#
+# by hyprtk (Kori Tk) (2026)
+# ----------------------------------------------------- 
+
+wal -q -i ~/Pictures/Wallpapers/ 
+
+killall waybar 2>/dev/null; ~/endeavour-dots/waybar/launch.sh
+
+~/endeavour-dots/papirus-icons/scripts/change-icons.sh
+
+exit
